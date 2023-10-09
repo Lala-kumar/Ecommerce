736704fcf6dde9fd6f716d1447e435da732b8d53
Lala Kumar <lalak4099@gmail.com>
2023-10-09 22:51:58 +0530

    Added desktop images as HeroSection.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
+import HeroSection from "../../components/heroSection/HeroSection";
 
 const Home = () => {
   return (
     <>
       <Layout>
-        <p>Home</p>
+        <HeroSection/>
       </Layout>
     </>
   );
