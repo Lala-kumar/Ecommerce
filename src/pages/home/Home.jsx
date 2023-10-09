23a5f2a79108e7498326340c9d514bf0e18693e6
Lala Kumar <lalak4099@gmail.com>
2023-10-09 23:31:57 +0530

    Added Filter below desktop image

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
 import HeroSection from "../../components/heroSection/HeroSection";
+import Filter from "../../components/filter/Filter";
 
 const Home = () => {
   return (
     <>
       <Layout>
-        <HeroSection/>
+        <HeroSection />
+        <Filter />
       </Layout>
     </>
   );
