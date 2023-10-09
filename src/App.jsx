f40aa4404dad7efac054bc7811175cd224882cf2
Lala Kumar <lalak4099@gmail.com>
2023-10-09 13:33:56 +0530

    added components and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <>
-      <p className="text-3xl font-bold text-center">hello</p>
-    </>
-  );
-}
+import React from "react";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Order from "./pages/order/Order";
+import Cart from "./pages/cart/Cart";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
+import NoPage from "./pages/nopage/NoPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/order", element: <Order /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/dashboard", element: <Dashboard /> },
+  { path: "/*", element: <NoPage /> },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
