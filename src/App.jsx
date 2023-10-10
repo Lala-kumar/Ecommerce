8832d2950e8e4bed0064886a8fdf2ebcd4e3b3ee
Lala Kumar <lalak4099@gmail.com>
2023-10-10 11:29:44 +0530

    Added Login & Signup page to the route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./pages/home/Home";
 import Order from "./pages/order/Order";
 import Cart from "./pages/cart/Cart";
 import Dashboard from "./pages/admin/dashboard/Dashboard";
+import Login from "./pages/registration/Login";
+import Signup from "./pages/registration/Signup";
 import NoPage from "./pages/nopage/NoPage";
 
 import MyState from "./context/myState";
@@ -14,6 +16,8 @@ const router = createBrowserRouter([
   { path: "/order", element: <Order /> },
   { path: "/cart", element: <Cart /> },
   { path: "/dashboard", element: <Dashboard /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
   { path: "/*", element: <NoPage /> },
 ]);
 
