23651184b466edac5eed4316ea62302a7b42f682
Lala Kumar <lalak4099@gmail.com>
2023-10-10 16:56:35 +0530

    Added cart to the file

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import myContext from "../../context/myContext";
 import Layout from "../../components/layout/Layout";
+import Modal from "../../components/modal/Modal";
 
 const Cart = () => {
   const context = useContext(myContext);
@@ -139,12 +140,7 @@ const Cart = () => {
             </div>
 
             {/* <Modal  /> */}
-            <button
-              type="button"
-              className="w-full  bg-violet-600 py-2 text-center rounded-lg text-white font-bold "
-            >
-              Buy Now
-            </button>
+            <Modal />
           </section>
         </div>
       </main>
