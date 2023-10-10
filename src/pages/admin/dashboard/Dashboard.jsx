543c3459228bdb9596c75699418c83adee0aa984
Lala Kumar <lalak4099@gmail.com>
2023-10-10 22:35:50 +0530

    Added dashboard Tabs with react tabs

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { FaUserTie } from "react-icons/fa";
 import myContext from "../../../context/myContext";
 import Layout from "../../../components/layout/Layout";
+import DashboardTab from "./DashboardTab";
 
 const Dashboard = () => {
   const context = useContext(myContext);
@@ -132,6 +133,7 @@ const Dashboard = () => {
             </div>
           </section>
         </div>
+        <DashboardTab/>
       </section>
     </Layout>
   );
